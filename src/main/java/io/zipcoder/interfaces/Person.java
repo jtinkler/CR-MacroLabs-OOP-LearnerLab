f7ada60fe7c969bb93e1da78cc5d8bf576f8cd34
Jennifer Tinkler <jennifertinkler@zipcoders-MacBook-Pro-94.local>
2018-11-05 18:24:34 -0500

    up to 3.1 completed.

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,29 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+            final long id;
+            String name;
+
+
+            Person(long id) {
+
+
+                this.id = id;
+
+            }
+
+            long getId(){
+                return id;
+
+            }
+
+            String getName() {
+                return name;
+            }
+
+            void setName(String name) {
+
+                this.name = name;
+            }
+
 }
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,44 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+
+     public void testConstructor() {
+        //given
+        long expectedId = 2L;
+        //when
+        Person jen = new Person(expectedId);
+
+        long actualId = jen.getId();
+        //then
+        Assert.assertEquals(expectedId, actualId);
+
+
+
+    }
+
+    @Test
+
+    public void  testSetName() {
+
+        //give
+        String expectedName = "Jen";
+        //when
+        Person jen = new Person(3L);
+
+        jen.setName(expectedName);
+
+        String actualName = jen.getName();
+        //then
+        Assert.assertEquals(expectedName, actualName);
+
+
+    }
+
+    
 
 }
